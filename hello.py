85774492153fff3a1d7663134ff51e7eb662a0f4
Rainey <59759137+raineyfu@users.noreply.github.com>
2022-09-27 03:06:21 -0400

    ex4-7

--- hello.py
+++ hello.py
@@ -3,10 +3,31 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import os
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 app = Flask(__name__)
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('user.html', form=form, name=name, current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
