69e20091862bb7196bb3f6041a375493627e23ca
Rainey <59759137+raineyfu@users.noreply.github.com>
2022-09-27 03:24:23 -0400

    ex4-7

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -12,6 +12,7 @@ import os
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
+    
 app = Flask(__name__)
 
 SECRET_KEY = os.urandom(32)
@@ -25,7 +26,11 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
         name = form.name.data
+        session['name'] = name
         form.name.data = ''
     return render_template('user.html', form=form, name=name, current_time = datetime.utcnow())
 
