b43a3471fd50b9bc43b524de35f252c2361945fe
Rainey <59759137+raineyfu@users.noreply.github.com>
2022-09-27 03:48:54 -0400

    last part

--- hello.py
+++ hello.py
@@ -2,17 +2,22 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-
+import re
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email
 
 import os
 
+def checkEmail(form, field):
+    if ("utoronto" not in str(field)):
+        raise ValidationError("Email must be from UofT")
+
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("What is your email?", validators=[DataRequired(), Email(), checkEmail])
     submit = SubmitField("Submit")
-    
+
 app = Flask(__name__)
 
 SECRET_KEY = os.urandom(32)
@@ -20,10 +25,11 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 @app.route("/", methods=["GET", "POST"])
 def index():
     name = None
+    email =None
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get("name")
@@ -32,7 +38,15 @@ def index():
         name = form.name.data
         session['name'] = name
         form.name.data = ''
-    return render_template('user.html', form=form, name=name, current_time = datetime.utcnow())
+
+        old_email = session.get("email")
+        if (old_email is not None and old_email != form.email.data):
+            flash("Looks like you have changed your email")
+
+        email = form.email.data
+        session['email'] = email
+        form.email.data = ''
+    return render_template('user.html', form=form, name=name, current_time = datetime.utcnow(), email=email)
 
 @app.route('/user/<name>')
 def user(name):
